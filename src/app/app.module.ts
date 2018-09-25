20bcd6db73a24c294bbd0c3ccf148516008c0549
Alaude30 <kuramamode11@gmail.com>
2018-09-25 23:09:34 +0800

    add component & styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,26 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/module/material.module';
 import { NavbarComponent } from './shared/component/navbar/navbar.component';
+
+// Pages
 import { HomeComponent } from './pages/home/home.component';
 import { ComponentComponent } from './pages/component/component.component';
 import { CdkComponent } from './pages/cdk/cdk.component';
 
+// Shared Component
+import { HeaderComponent } from './shared/component/header/header.component';
+import { FooterComponent } from './shared/component/footer/footer.component';
+
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HomeComponent, ComponentComponent, CdkComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ComponentComponent,
+    CdkComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent]
