ca7f6d617cb41fa874653aa1b7eedf7a9974fa58
Alaude30 <kuramamode11@gmail.com>
2018-09-25 14:13:31 +0800

    update component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ComponentComponent } from './pages/component/component.component';
+import { CdkComponent } from './pages/cdk/cdk.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'cdk', component: CdkComponent },
+  { path: 'component', component: ComponentComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
