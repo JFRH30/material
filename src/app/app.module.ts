3837010a135cbb5df39aca059d4ff921eb622087
Alaude30 <kuramamode11@gmail.com>
2018-09-25 14:14:38 +0800

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,13 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/module/material.module';
+import { NavbarComponent } from './shared/component/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ComponentComponent } from './pages/component/component.component';
+import { CdkComponent } from './pages/cdk/cdk.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, ComponentComponent, CdkComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent]
