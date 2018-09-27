b687ff79883ab2d09fb59e61833f150db0161144
Alaude30 <kuramamode11@gmail.com>
2018-09-27 13:31:53 +0800

    add viewer for docs

---
+++ src/app/shared/component/api-viewer/api-viewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-api-viewer',
+  templateUrl: './api-viewer.component.html',
+  styleUrls: ['./api-viewer.component.scss']
+})
+export class ApiViewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
