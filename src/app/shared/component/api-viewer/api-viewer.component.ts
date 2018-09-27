b141a3409b099ad68d4dbfdd79e17ac9cd7ec928
Alaude30 <kuramamode11@gmail.com>
2018-09-27 19:20:06 +0800

    finished navigation for api docs

--- src/app/shared/component/api-viewer/api-viewer.component.ts
+++ src/app/shared/component/api-viewer/api-viewer.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MaterialComponent } from '../../../model/material-component.model';
+import { ActivatedRoute } from '@angular/router';
+// import { routes } from '../../../data/routes.data';
+import { MaterialRoutes } from '../../../model/material-routes.model';
+import { routes } from '../../../data/routes.data';
 
 @Component({
   selector: 'app-api-viewer',
@@ -6,10 +11,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./api-viewer.component.scss']
 })
 export class ApiViewerComponent implements OnInit {
-
-  constructor() { }
+  components: MaterialComponent[];
+  routes = [];
+  control = false;
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.routes = routes;
+    for (const route of this.routes) {
+      for (const subRoute of route.subRoutes) {
+        if (this.route.snapshot.paramMap.get('component') === subRoute.path) {
+          this.components = subRoute.data;
+          console.log(this.components);
+        }
+      }
+    }
+    this.route.params.subscribe(res => {
+      for (const route of this.routes) {
+        for (const subRoute of route.subRoutes) {
+          if (res.component === subRoute.path) {
+            this.components = subRoute.data;
+            console.log(this.components);
+          }
+        }
+      }
+    });
   }
-
 }
