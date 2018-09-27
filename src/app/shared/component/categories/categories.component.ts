ea4996592a642787ec3b1daf737fd18c97d0ecda
Alaude30 <kuramamode11@gmail.com>
2018-09-27 19:20:50 +0800

    finished navigation

--- src/app/shared/component/categories/categories.component.ts
+++ src/app/shared/component/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { routes } from '../../../data/routes.data';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-categories',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.scss']
 })
 export class CategoriesComponent implements OnInit {
+  routes = [];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    console.log(this.route.snapshot.paramMap.get('item'));
+    this.routes = routes;
+    for (const route of this.routes) {
+      if (this.route.snapshot.paramMap.get('item') === route.path) {
+        this.routes = route.subRoutes;
+      }
+    }
+    this.route.params.subscribe(res => {
+      for (const route of this.routes) {
+        if (res.item === route.path) {
+          this.routes = route.subRoutes;
+        }
+      }
+    });
   }
-
 }
